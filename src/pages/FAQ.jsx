fa303af5d3974e3e317d9581df069a0d2ec3230e
Nicholas Brown <nicholas.brown@jpl.nasa.gov>
2025-06-11 08:00:00 -0700

    Fix mobile view for FAQs

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -25,7 +25,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">What is the Armada Ruleset Collective (ARC)?</h2>
               <span className="text-white text-2xl">{openIndex === 0 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 0 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 0 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 ARC is an initiative by Armada players who have come together to ensure Star Wars Armada continues to thrive after AMG withdrew support for this game. Our team includes national and international tournament winners, Tournament Organizers, former FFG playtesters, and prominent community members.
                 <br /><br />
@@ -48,7 +52,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">What is ARC's philosophy and approach towards sustaining Armada?</h2>
               <span className="text-white text-2xl">{openIndex === 1 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 1 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 1 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 ARC takes a long-term holistic view when considering the actions needed to ensure Armada flourishes. We will need to ensure that our approach to Rules, Organized Play, and balance remains open and sustainable, and that any changes to the game are measured and appropriately paced. New content for Armada has historically been introduced relatively slowly and the pace of change should be actively managed. We are in a time of transition for Armada, and the change from a store-purchased game to a community-sourced one will be a learning process. This is one reason why our first content release will not require a purchase of new models or ship tokens; until widespread 3D printing is more easily available the content should be accessible to as many players as possible.
                 <br /><br />
@@ -66,7 +74,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">Has ARC changed the game? I heard something about rules changes last fall?</h2>
               <span className="text-white text-2xl">{openIndex === 2 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 2 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 2 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 No, ARC has not made any changes to the game rules. The Armada you know and love is the Armada we intend to support and continue. The rules interpretations in the ARC-ARM (our errata document) are 99.9% the same as before, with a few obscure rulings tweaked for internal rules consistency. In the future we may alter some basic rules if we feel the need arises, but we will do so with utmost caution. At this time we have no plans to do so; the suggested changes in a previous fall tournament were an experiment and have no relation to our current plans.
               </p>
@@ -82,7 +94,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">Does this mean there will be no changes to current cards or rules then?</h2>
               <span className="text-white text-2xl">{openIndex === 3 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 3 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 3 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 No, cards have been altered and changed throughout the history of Armada for errata and balance purposes. We intend to continue this tradition, adjusting point values and providing erratas to encourage a variety of viable play styles and fleet compositions. As stated in our Guiding Principles in Dec. 2024, we will strive to buff rather than nerf, and will make changes with the goal of keeping the game lively and balanced. To quote the Guiding Principles, "...stagnation is deadly for a modern game system with a competitive scene. Injecting interest via point changes, errata, and some new content will help keep interest alive."
               </p>
@@ -98,7 +114,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">So ARC will be releasing new content?</h2>
               <span className="text-white text-2xl">{openIndex === 4 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 4 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 4 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 ARC will release new content when necessary to keep the game fresh and maintain balance and interest for the community. Our focus will always include considerations of how new content will affect faction identity, future design space, and the state of the game as a whole.
               </p>
@@ -114,7 +134,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">What is the difference between ARC and other community project such as Legacy or Nexus?</h2>
               <span className="text-white text-2xl">{openIndex === 5 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 5 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 5 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 The primary goal of ARC is to steward the rules and mechanics of this game we all love—that's why we chose the name Ruleset Collective. ARC was founded on the idea of maintaining balance and supporting organized play. The means of achieving that goal might involve new content or balance patches, but also includes creating resources like the <b><u><a href="https://armadarulesetcollective.com/rules" target="_blank">Armada Reference Manual</a></u></b>. to help tournament organizers and community leaders run events smoothly. In addition ARC is organizing an exciting Organized Play season for the coming year, continuing the best traditions established by FFG and AMG. ARC is communicating with Legacy and Nexus to see where we can align our goals and operations, and support Armada cooperatively.
               </p>
@@ -130,7 +154,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">What will organized play look like in the future?</h2>
               <span className="text-white text-2xl">{openIndex === 6 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 6 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 6 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 ARC is partnering with major events around the world and online, including significant European, North American, and Australian tournaments. We are committed to continuing the Organized Play circuit that the community has come to love, culminating in a major event at the end of each season.
 
@@ -148,7 +176,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">Will there be new prize support or tournament kits available?</h2>
               <span className="text-white text-2xl">{openIndex === 7 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 7 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 7 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 Yes! We've partnered with creators from around the world to develop new and exciting tournament kits for organizers. These updated kits are designed to be even more engaging than those from previous Organized Play seasons.
               </p>
@@ -164,7 +196,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">What is the ARC Partnered event?</h2>
               <span className="text-white text-2xl">{openIndex === 8 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 8 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 8 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 An ARC Partnered Event is an Armada Organized Play event that follows the latest ARC ruleset. This includes rulings from the <b><u><a href="https://armadarulesetcollective.com/rules" target="_blank">Armada Reference Manual</a></u></b> and any new content released during the event's eligibility window. These events will be listed on our website and are designed to offer a consistent play experience across the ARC Organized Play Circuit. Additionally, ARC Partnered Events will have access to that season's ARC tournament kits, ensuring a high-quality, standardized tournament experience.
               </p>
@@ -180,7 +216,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">Can I host an ARC Partnered event?</h2>
               <span className="text-white text-2xl">{openIndex === 9 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 9 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 9 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 The application process is still in development; we are working toward providing a clear and accessible system for tournament organizers interested in hosting ARC Organized Play events. In the meantime please reach out to us at <b><u><a href="mailto:op@armadarulesetcollective.com">op@armadarulesetcollective.com</a></u></b> with any questions related to Organized Play or to express your interest in hosting an event.
               </p>
@@ -196,7 +236,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">Will ARC approve or use community content for the competitive ruleset?</h2>
               <span className="text-white text-2xl">{openIndex === 10 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 10 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 10 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 With the creator's approval, ARC may incorporate or endorse community content in the competitive ruleset if it fills an identified need in the game. Cards will be evaluated for balance and usability, and will undergo rigorous playtesting and review.
               </p>
@@ -212,7 +256,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">How will ARC ensure new content is balanced and fair for both competitive and casual players?</h2>
               <span className="text-white text-2xl">{openIndex === 11 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 11 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 11 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 ARC conducts thorough playtesting with a committed team of testers and community members. This includes multiple testing rounds across various skill levels, factions, and playstyles. Our team features former FFG employees and testers, World class players, and casual players alike. Many have been playing since the game was released and some are relatively new to Armada. We always evaluate both the item in question and its potential impact on the game as a whole. This diversity is the key to our confidence in the balance and fairness of our content.
               </p>
@@ -228,7 +276,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">How can I stay updated with ARC news?</h2>
               <span className="text-white text-2xl">{openIndex === 12 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 12 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 12 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 You can stay informed by visiting our website's <b><u><a href="https://armadarulesetcollective.com/news" target="_blank">news page</a></u></b>, joining the Armada Hub Discord server, or following us on Facebook and Reddit. We regularly post updates about new features, rule clarifications, and community events. Come check us out!
               </p>
@@ -244,7 +296,11 @@ const FAQ = () => {
               <h2 className="text-xl font-semibold text-white">Is ARC affiliated with Fantasy Flight Games or Atomic Mass Games?</h2>
               <span className="text-white text-2xl">{openIndex === 13 ? '−' : '+'}</span>
             </button>
-            <div className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${openIndex === 13 ? 'max-h-96 py-4' : 'max-h-0'}`}>
+            <div 
+              className={`px-6 overflow-hidden transition-all duration-300 ease-in-out ${
+                openIndex === 13 ? 'max-h-[2000px] py-4' : 'max-h-0'
+              }`}
+            >
               <p className="text-gray-300 leading-relaxed whitespace-pre-line">
                 No. ARC is an independent community initiative and is not affiliated with Fantasy Flight Games, Atomic Mass Games, Lucasfilm Ltd., or any other company. We are a not-for-profit organization built by and for the community.
               </p>
